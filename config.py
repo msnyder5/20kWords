7a5f058664750ed5c7717c55d5caef6cc8f16709
Matthew Snyder <msnyder2@terpmail.umd.edu>
2022-05-07 09:21:32 -0400

    added config, multiple files at once, more pregenerated outputs

---
+++ config.py
@@ -0,0 +1,39 @@
+# # # # # # # # # # # # # # # # # # # # # # #
+#      The sort order for output files      #
+# # # # # # # # # # # # # # # # # # # # # # #
+# 1 - Same order as input file              #
+# 2 - Alphabetical                          #
+# 3 - Length (shortest to longest)          #
+# 4 - Status                                #
+# 5 - Registration date (newest to oldest)  #
+# 6 - Registration date (oldest to newest)  #
+# 7 - Expiry date (newest to oldest)        #
+# 8 - Expiry date (oldest to newest)        #
+# # # # # # # # # # # # # # # # # # # # # # #
+SORTBY = 8
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # #
+#               Toggle Outputs              #
+# # # # # # # # # # # # # # # # # # # # # # #
+# available.txt
+# Note: only saved if there are any domains available
+AVAILABLE = True
+# # # # # # # # # # # # # # # # # # # # # # #
+# domains.csv
+CSV = True
+# # # # # # # # # # # # # # # # # # # # # # #
+# valid.txt
+# Note: only saved if there are invalid domains
+VALID = True
+# # # # # # # # # # # # # # # # # # # # # # #
+# /length/n char
+# Note: only saved if the collection is of multiple lengths
+LENGTH = True
+# # # # # # # # # # # # # # # # # # # # # # #
+# README.md
+# Note: only saved for 20kWordClub.txt
+#       doesn't really matter for end user
+README = True
+# # # # # # # # # # # # # # # # # # # # # # #
--- search.py
+++ search.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import os
 import re
 import grequests
@@ -6,12 +7,8 @@ import datetime
 from Crypto.Hash import keccak
 from dataclasses import dataclass
 
-INPUT = './input/top20kwords.txt'
-AVAILABLE = 'available'
-NAMES = 'names'
-LETTERS = '{} letters'
-VALIDWORDS = 'validwords'
-
+from tqdm import tqdm
+import config
 
 URL = "https://api.thegraph.com/subgraphs/name/ensdomains/ens"
 QUERY = "\n        query getName($ids: [ID!]) {\n          registrations(where: { id_in: $ids }) {\n            id\n            labelName\n            expiryDate\n            registrationDate\n          }\n        }\n    "
@@ -61,17 +58,17 @@ class ENSListing():
 
     # Get line string for csv output
     def getcsv(self):
+        if self._enstype in [ENSType.NEW, ENSType.EXPIRED]: url = f"https://app.ens.domains/name/{self.name}.eth/register"
+        else: url = f"https://opensea.io/assets/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85/{self._decid}"
         attrs = [self.name,
                  len(self.name),
                  self._enstype.value,
                  self._premium,
+                 url,
                  self.registrationdate,
                  self.expirydate,
                  self._gracedate,
-                 self._premiumdate,
-                 self._hexid,
-                 self._decid,
-                 f"'{self._decid}'"]
+                 self._premiumdate]
         return ",".join([str(i) if i else '' for i in attrs])
 
 def getids(name: str, hexonly=False):
@@ -83,10 +80,10 @@ def getids(name: str, hexonly=False):
     return f"0x{hex}", dec
 
 # Get a list of all of the valid words.
-def getwords():
-    with open(INPUT) as txtfile:
+def getwords(dirname: str):
+    with open(f"./input/{dirname}.txt") as txtfile:
         lines = txtfile.readlines()
-    print(f"\nBulk searching {len(lines)} words...\n")
+    print(f"\nBulk searching {len(lines)} words in {dirname}.txt...")
     words = [line[:-1] if '\n' in line else line for line in lines]
     validwords = [name for name in words if len(name) > 2]
     return validwords, len(words)-len(validwords)
@@ -106,11 +103,24 @@ def getlistingdata(words: list[str]):
     datachunks = [r.json()["data"]["registrations"] for r in responses]
     return [i for chunk in datachunks for i in chunk]
 
+def sortdomains(words: list[str], domainobjs: list[ENSListing]):
+    wordsranks = {d: i for i, d in enumerate(words)}
+    domainobjs.sort(key=lambda x: wordsranks[x.name])
+    if config.SORTBY == 2: domainobjs.sort(key=lambda x: x.name)
+    elif config.SORTBY == 3: domainobjs.sort(key=lambda x: len(x.name))
+    elif config.SORTBY == 4: domainobjs.sort(key=lambda x: x._enstype.value)
+    elif config.SORTBY == 5: domainobjs.sort(key=lambda x: x.registrationdate or datetime.datetime.min, reverse=True)
+    elif config.SORTBY == 6: domainobjs.sort(key=lambda x: x.registrationdate or datetime.datetime.max)
+    elif config.SORTBY == 7: domainobjs.sort(key=lambda x: x.expirydate or datetime.datetime.min, reverse=True)
+    elif config.SORTBY == 8: domainobjs.sort(key=lambda x: x.expirydate or datetime.datetime.max)
+    return domainobjs
+
 # Create the domain object for each domain
 def getdomains(words: list[str], domaindata: list[dict]):
     # Add New Domains
     returnednames = [word["labelName"] for word in domaindata]
-    domains = [ENSListing(word) for word in words if word not in returnednames]
+    newdomains = list(set(words) - set(returnednames))
+    domains = [ENSListing(word) for word in newdomains]
     # Add Other Domains
     def generateenslisting(entry: dict):
         name = entry["labelName"]
@@ -118,59 +128,69 @@ def getdomains(words: list[str], domaindata: list[dict]):
         registrationdt = datetime.datetime.fromtimestamp(float(entry["registrationDate"]))
         return ENSListing(name, expirydt, registrationdt)
     domains += [generateenslisting(entry) for entry in domaindata]
-    return domains
+    return sortdomains(words, domains)
 
-def makeoutputdir():
-    if not os.path.exists("./output"): os.mkdir("./output")
-    if not os.path.exists("./output/letters"): os.mkdir("./output/letters")
+def makeoutputdir(dirname: str):
+    if dirname == '20kWordClub':
+        if not os.path.exists("./20kWordClub"): os.mkdir("./20kWordClub")
+        if not os.path.exists("./20kWordClub/length"): os.mkdir("./20kWordClub/length")
+        os.chdir(f'./20kWordClub')
+    else:
+        if not os.path.exists("./output"): os.mkdir("./output")
+        if not os.path.exists(f"./output/{dirname}"): os.mkdir(f"./output/{dirname}")
+        if not os.path.exists(f"./output/{dirname}/length"): os.mkdir(f"./output/{dirname}/length")
+        os.chdir(f'./output/{dirname}')
 
 # Saves a text file with all the non-premium words
 def saveavailable(domains: list[ENSListing]):
-    enslist = [i.name for i in domains if i._enstype is ENSType.NEW or i._enstype is ENSType.EXPIRED]
-    with open(f"./output/{AVAILABLE}.txt", 'w') as file:
+    enslist = [i.name for i in domains if i._enstype in [ENSType.NEW, ENSType.EXPIRED]]
+    if not enslist: return
+    with open(f"available.txt", 'w') as file:
         file.write("\n".join(enslist))
 
 # Saves valid words and words seperated by length
-def savewords(words: list[str]):
-    with open(f"./output/{VALIDWORDS}.txt", 'w') as file:
+def savevalid(words: list[str]):
+    with open(f"valid.txt", 'w') as file:
         file.write("\n".join(words))
-    length = 3
-    counted = 0
-    total = len(words)
-    while(counted < total):
-        validwords = [word for word in words if len(word) == length]
-        with open(f"./output/letters/{LETTERS}.txt".format(length), 'w') as file:
-            file.write("\n".join(validwords))
-        counted += len(validwords)
-        length += 1
+
+def savelength(words: list[str]):
+    lengthdict = defaultdict(list)
+    for word in words: lengthdict[len(word)].append(word)
+    if len(lengthdict) < 2:
+        os.rmdir('./length')
+        return
+    for i, iwords in lengthdict.items():
+        with open(f"./length/{i} chars.txt", 'w') as file:
+            file.write("\n".join(iwords))
 
 # Saves a CSV containing data for all of the domains provided
 def savemaincsv(domains: list[ENSListing]):
-    enslist = ["Name,Length,Status,Premium,Registered,Expires,Grace Period Ends,Price Premium Ends,Hex Token ID,Decimal Token ID,Decimal Token ID Repr"]
+    enslist = ["Name,Length,Status,Premium,URL,Registered,Expires,Grace Period Ends,Price Premium Ends"]
     enslist += [i.getcsv() for i in domains]
-    with open(f"./output/{NAMES}.csv", 'w') as file:
+    with open(f"domains.csv", 'w') as file:
         file.write("\n".join(enslist))
 
 # Update readme and print summary
 def readmeandprint(start: datetime.datetime,
                    words: list[str],
                    numinvalid: int,
-                   domainobjs: list[ENSListing]):
+                   domainobjs: list[ENSListing],
+                   update = False):
     numvalid = len(words)
     numavailable = len([i.name for i in domainobjs if i._enstype is ENSType.NEW or i._enstype is ENSType.EXPIRED])
     numpremium = len([d for d in domainobjs if d._enstype is ENSType.PREMIUM])
     pastday = len(['' for i in domainobjs if i.registrationdate and i.registrationdate > (datetime.datetime.now()-datetime.timedelta(days=1))])
     elapsed = (datetime.datetime.now() - start).total_seconds()
     totalwords = numvalid+numinvalid
-    updatereadme(numavailable, pastday)
-    print(f"ENS bulk search completed in {elapsed:.2f} seconds.\n"
-          "Files have been outputted to ./output\n"
-          f"{totalwords} Words Searched\n"
-          f"{numvalid} Valid ({(numvalid*100.0)/totalwords:.2f}%)\n"
-          f"{numinvalid} Invalid ({(numinvalid*100.0)/totalwords:.2f}%)\n"
-          f"{numavailable} Available for base price ({(numavailable*100.0)/numvalid:.2f}%)\n"
-          f"{numpremium} Available for premium price ({(numpremium*100.0)/numvalid:.2f}%)\n"
-          f"{pastday} Registered in the past day ({(pastday*100.0)/(numavailable+pastday):.2f}%)\n")
+    if update: updatereadme(numavailable, pastday)
+    printstr = (f"ENS bulk search completed in {elapsed:.2f} seconds.\n"
+                f"{totalwords} Words Searched\n"
+                f"{numvalid} Valid ({(numvalid*100.0)/totalwords:.2f}%)\n"
+                f"{numinvalid} Invalid ({(numinvalid*100.0)/totalwords:.2f}%)\n")
+    if numavailable: printstr += f"{numavailable} Available for base price ({(numavailable*100.0)/numvalid:.2f}%)\n"
+    if numpremium: printstr += f"{numpremium} Available for premium price ({(numpremium*100.0)/numvalid:.2f}%)\n"
+    if numavailable: printstr += f"{pastday} Registered in the past day ({(pastday*100.0)/(numavailable+pastday):.2f}%)\n"
+    print(printstr)
 
 # Autoupdate readme with new statistics (can be disabled)
 def updatereadme(numavailable: int, pastday: int):
@@ -183,20 +203,22 @@ def updatereadme(numavailable: int, pastday: int):
         readme.write(rmstr)
 
 def main():
-    start = datetime.datetime.now()
     # Getting data
-    words, numinvalid = getwords()
-    domaindata = getlistingdata(words)
-    domainobjs = getdomains(words, domaindata)
-    # Sort domains by commonality
-    wordsranks = {d: i for i, d in enumerate(words)}
-    domainobjs.sort(key=lambda x: wordsranks[x.name])
-    # Outputs
-    makeoutputdir()
-    saveavailable(domainobjs)
-    savewords(words)
-    savemaincsv(domainobjs)
-    readmeandprint(start, words, numinvalid, domainobjs)
+    for filename in os.listdir('./input'):
+        dirname = filename.removesuffix('.txt')
+        start = datetime.datetime.now()
+        words, numinvalid = getwords(dirname)
+        domaindata = getlistingdata(words)
+        domainobjs = getdomains(words, domaindata)
+        # Outputs
+        makeoutputdir(dirname)
+        if config.AVAILABLE: saveavailable(domainobjs)
+        if config.VALID and numinvalid: savevalid(words)
+        if config.LENGTH: savelength(words)
+        if config.CSV: savemaincsv(domainobjs)
+        os.chdir('..') if dirname == '20kWordClub' else os.chdir('../..')
+        update = config.README and dirname == '20kWordClub'
+        readmeandprint(start, words, numinvalid, domainobjs, update)
 
 if __name__ == '__main__':
     main()
